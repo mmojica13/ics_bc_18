2c137042243d5a226ff29ca448b23d5771d41093
mmojica13 <mmojica@berkeley.edu>
2018-06-20 22:16:36 -0700

    Week1

---
+++ week1/ch02/age_in_seconds.rb
@@ -0,0 +1,1 @@
+puts "I am eighteen years old, so I am " + (18*31557600 - 4*86400).to_s + " seconds old!"
---
+++ week1/ch05/bigger_better_favorite_number.rb
@@ -0,0 +1,4 @@
+puts "What is your favorite number?"
+number = gets.chomp
+better = number.to_i + 1
+puts "That's a pretty cool number... but " + better.to_s + " is a better favorite number...:/"
