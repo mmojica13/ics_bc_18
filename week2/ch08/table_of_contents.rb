306202d589312c0fb8c9568225d6e1a2cf6e93d5
mmojica13 <40327618+mmojica13@users.noreply.github.com>
2018-06-22 19:00:08 -0700

    Delete table_of_contents.rb

--- week2/ch08/table_of_contents.rb
+++
@@ -1,6 +0,0 @@
-#this is the table of contents assignment for ch06
-line_width = 50
-str_title = "Title"
-ch1 = "Chapter 1:" + line_width.to_s + "What is GitHub" + line_width
-puts (str_title.center( line_width))
-puts " "
