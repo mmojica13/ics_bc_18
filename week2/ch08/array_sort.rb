1c2ef21f0b999f35e1c557392409ebf9923df27c
mmojica13 <mmojica@berkeley.edu>
2018-06-22 18:55:22 -0700

    complete array_sort

--- week2/ch08/array_sort.rb
+++ week2/ch08/array_sort.rb
@@ -1,11 +1,12 @@
-puts "This program will take words and put them in alphabetical order."
-puts "Type a words and then press enter."
+puts "Got a bunch of words that need sorting? Let me put them in alphabetical order!"
+puts "Type some words and then press enter."
 puts " When you are done, please press enter on an empty line."
 puts " "
 
 array = []
 while (input_word = gets.chomp) != ""
   array.push input_word
-  puts "Please hold while we sort out the mess."
 end
-print "Here is the alphabetized list: " + array.sort.join(', ') + "."
+puts "Here is the alphabetized list: "
+puts array.sort.join(', ')
+puts " "
