2839182ecc58dd76397ccd93997dea28d6a6a6f3
mmojica13 <mmojica@berkeley.edu>
2018-06-22 18:48:13 -0700

    complete ch06 table_of_contents

---
+++ week2/ch06/table_of_contents_ch06.rb
@@ -0,0 +1,6 @@
+page_width = 60
+puts ("Table of Contents".center(page_width))
+puts " "
+puts ("Chapter 1: What is GitHub?".ljust(page_width/2) + "page 1".rjust(page_width/2.05))
+puts ("Chapter 2: No...SERIOUSLY, What is this??!".ljust(page_width/2) + "page 9".rjust(page_width/3.5))
+puts ("Chapter 3: HELP!!!".ljust(page_width/2) + "page 13".rjust(page_width/2))
