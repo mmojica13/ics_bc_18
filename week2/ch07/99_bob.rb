15d9a15550fb1ce3a5afa834a73e9b9b7ade8b48
mmojica13 <mmojica@berkeley.edu>
2018-06-22 18:50:26 -0700

    complete 99_bob

--- week2/ch07/99_bob.rb
+++ week2/ch07/99_bob.rb
@@ -1,12 +1,11 @@
 puts "Let's sing the 99 Bottles of Beer song!"
 bottles = 99
-while bottles != 0
-  puts bottles.to_s + " bottles of beer on the wall! " + bottles.to_s + " bottles of beer!"
-  bottles_minus = bottles - 1
-  puts "You take one down, pass it around! " + bottles_minus.to_s + " bottles of beer of the wall!"
-  puts " "
-  if bottles == 0
-    puts "No more bottles of beer on the wall, no more bottles of beer!"
-    puts "Go to the store and buy some more, 99 bottles of beer on the wall!!! ;)"
-  end
+bottles_now = bottles
+while bottles_now != 0
+    puts bottles_now.to_s + " bottles of beer on the wall! " + bottles_now.to_s + " bottles of beer!"
+    bottles_now = bottles_now - 1
+    puts "You take one down, pass it around! " + bottles_now.to_s + " bottles of beer of the wall!"
+    puts " "
 end
+puts "No more bottles of beer on the wall, no more bottles of beer!"
+puts "Go to the store and buy some more, 99 bottles of beer on the wall!!! ;)"
