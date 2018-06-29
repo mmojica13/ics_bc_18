30955b650414d14f1136e1de0299db8ce8b2a5f4
mmojica13 <mmojica@berkeley.edu>
2018-06-28 21:51:57 -0700

    ask.rb done

--- week3/ch09/new_roman_numerals.rb
+++ week3/ch09/new_roman_numerals.rb
@@ -3,18 +3,18 @@ def new_roman_numeral num
   hunds = (num % 1000 / 100)
   tens = (num % 100 / 10)
   ones = (num % 10)
-  roman = "M" * thous   #this calculates the thousands in M
-  if hunds == 9             #this is the case for the before style for 9
+  roman = "M" * thous    #this calculates the thousands in M
+  if hunds == 9    #this is the case for the before style for 9
     roman = roman + "CM"
-  elsif hunds == 4          #this is the case for before style for 4
+  elsif hunds == 4    #this is the case for before style for 4
     roman = roman + "CD"
   else
     roman = roman + "D" * (num % 1000 / 500)
     roman = roman + "C" * (num % 500 / 100)
   end
-  if tens == 9             #before style for 9
+  if tens == 9    #before style for 9
     roman = roman + "XC"
-  elsif tens == 4           #before style for 4
+  elsif tens == 4    #before style for 4
     roman = roman + "XL"
   else
     roman = roman + "L" * (num % 100 / 50)
@@ -28,6 +28,5 @@ def new_roman_numeral num
     roman = roman + "V" * (num % 10 / 5)
     roman = roman + "I" * (num % 5)
   end
-  roman
 end
-puts(new_roman_numeral(2897))
+puts(new_roman_numeral(2497))    #2497 replaces num 
--- week3/ch09/old_roman_numerals.rb
+++ week3/ch09/old_roman_numerals.rb
@@ -1,12 +1,11 @@
 def old_roman_numeral num
-  roman = " "
-  roman = roman + "M" * (num     / 1000)
-  roman = roman + "D" * (num % 1000 /  500)
-  roman = roman + "C" * (num % 500 /  100)
-  roman = roman + "L" * (num % 100 /  50)
-  roman = roman + "X" * (num % 50 /  10)
-  roman = roman + "V" * (num % 10 /  5)
-  roman = roman + "I" * (num % 5 /  1)
-  roman
+  roman = ""          #this allows us to turn the num in the designated roman letter
+  roman = roman + "M" * (num / 1000)    #thousands
+  roman = roman + "D" * (num % 1000 /  500)   #five hundred
+  roman = roman + "C" * (num % 500 /  100)   #one hundred
+  roman = roman + "L" * (num % 100 /  50)    #fifty
+  roman = roman + "X" * (num % 50 /  10)    #ten
+  roman = roman + "V" * (num % 10 /  5)    #five
+  roman = roman + "I" * (num % 5 /  1)    #one
 end
-puts old_roman_numeral(2897)
+puts old_roman_numeral(2497)    #2497 replaces num
