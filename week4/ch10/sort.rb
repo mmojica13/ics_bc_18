d8d6031fb598d4ae97dc97d7cedac34687b2ca0e
mmojica13 <mmojica@berkeley.edu>
2018-07-06 23:00:30 -0700

    sort ch10 done

--- week4/ch10/sort.rb
+++ week4/ch10/sort.rb
@@ -3,7 +3,19 @@ def sort(some_array) # This "wraps" recursive_sort.
 end
 
 def recursive_sort(unsorted_array, sorted_array)
-  # Your fabulous code goes here.
+  if unsorted_array.length <= 0 # Your fabulous code goes here.
+    return sorted_array
+  end
+
+  shortest = unsorted_array.pop
+  still_unsorted = []
+  unsorted_array.each do |test_object|
+    if test_object < shortest
+      shortest = test_object
+    end
+  end
+  sorted_array.push shortest
+  recursive_sort still_unsorted, sorted_array
 end
 
-sort [__, __, ...]
+puts sort ["noodle", "whales", "exhuasting", "throat"]
