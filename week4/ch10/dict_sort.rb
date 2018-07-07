2ab261fc67e01616b0594e8863fb968e0928e287
mmojica13 <mmojica@berkeley.edu>
2018-07-06 22:58:35 -0700

    dict_sort done

--- week4/ch10/dict_sort.rb
+++ week4/ch10/dict_sort.rb
@@ -1,9 +1,25 @@
 def dict_sort(some_array) # This "wraps" recursive_sort.
-  recursive_sort some_array, []
+  recursive_dict_sort some_array, [ ]
 end
 
 def recursive_dict_sort(unsorted_array, sorted_array)
-  # Your fabulous code goes here.
+  if unsorted_array.length <= 0
+    return sorted_array
+  end
+
+  smallest = unsorted_array.pop
+  still_unsorted = []
+
+  unsorted_array.each do |test_object|
+    if test_object.downcase < smallest.downcase
+      still_unsorted.push smallest
+      smallest = test_object
+    else
+      still_unsorted.push test_object
+    end
+  end
+  sorted_array.push smallest
+  recursive_dict_sort still_unsorted, sorted_array
 end
 
-dict_sort [__, __, ...]
+puts dict_sort ["noodle","hair","wink", "apple"]
