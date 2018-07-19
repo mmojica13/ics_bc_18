4e9849302fde9385d58208657452c3ab64c31051
mmojica13 <mmojica@berkeley.edu>
2018-07-19 16:42:53 -0700

    profiling

--- week6/ch14/profiling.rb
+++ week6/ch14/profiling.rb
@@ -1,6 +1,11 @@
 def profile block_description, &block
-  start_time = Time.new
-  block.call
-  duration = Time.new - start_time
-  puts "#{block_description}: #{duration} seconds"
+  profiling_on = false
+  if profiling_on == false
+    start_time = Time.new
+    block.call
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block.call
+  end
 end
