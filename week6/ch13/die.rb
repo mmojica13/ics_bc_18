f89913c3cbc845a6bf7454cbf6dcfa91e004437b
mmojica13 <mmojica@berkeley.edu>
2018-07-19 16:37:41 -0700

    die.rb

--- week6/ch13/die.rb
+++ week6/ch13/die.rb
@@ -1,15 +1,33 @@
 class Die
   def initialize
-    roll
+    puts "Do you want to cheat, or play fair? To cheat, type 'cheat' and press enter. Otherwise just press enter."
+    response = gets.chomp
+    if response == "cheat"
+      cheat
+    else
+      roll
+    end
   end
 
   def roll
-    @number_showing = rand(1..6)
+    @number_showing = rand(1..6) + 1
   end
 
   def showing
-    @number_showing
+    puts "You rolled... #{@number_showing}!"
   end
 
-  # Define a cheat method here
+  def cheat
+    puts "What do you want to roll? (1-6)"
+    number = gets.chomp.to_i
+    if number < 6 && number > 1
+      @number_showing = number
+    else
+      @number_showing = "nothing"
+      puts "That isn't between 1 and 6, cheater."
+    end
+  end
+  @number_showing
 end
+
+Die.new.showing
